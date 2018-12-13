ebc5e5b3e5b19253d4db1fc16fb7e627c8866f0f
Rose E Wang <rewang@dhcp-18-30-112-169.dyn.mit.edu>
2018-12-13 16:49:45 -0500

    [edited] experiments work with distance model

--- data/read_data.py
+++ data/read_data.py
@@ -10,16 +10,17 @@ def load_data(file_name):
   pickle_file.close()
 
   # Unnecessary. Just to show whats in the data.
-  agent1_pos = data["agent1_pos"]
-  agent1_heading = data["agent1_heading"]
-  agent2_pos = data["agent2_pos"]
-  agent2_heading = data["agent2_heading"]
-  is_collided = data["is_collided"]
-  full_obs = data["full_obs"]
+  # agent1_pos = data["agent1_pos"]
+  # agent1_heading = data["agent1_heading"] 
+  # agent2_pos = data["agent2_pos"]
+  # agent2_heading = data["agent2_heading"]
+  # is_collided = data["is_collided"]
+  # full_obs = data["full_obs"] 
 
   return data
 
 if __name__ == '__main__':
   file_name = "logs/obs_history_1"
-  load_data(file_name)
-  print(data)
+  data= load_data(file_name)
+  print(data["agent2_pos"][:1])
+  print(data["agent2_heading"][:1])
